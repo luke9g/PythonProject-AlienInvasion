1bfeb39c8b2fac617816fe04f49f81c4ac6d8567
Łukasz <lukas.vision@gmail.com>
2021-02-25 19:57:03 +0100

    Display current level

--- game_stats.py
+++ game_stats.py
@@ -27,3 +27,4 @@ class GameStats:
         """Inicjalizacja tych statystyk, które mogą zmieniać się w trakcie gry."""
         self.ships_left = self.settings.ship_limit
         self.score = 0
+        self.level = 1
--- scoreboard.py
+++ scoreboard.py
@@ -8,22 +8,22 @@ class Scoreboard:
         """Inicjalizacja atrybutów dotyczących punktacji."""
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
-        self.settings = ai_game.settings
         self.stats = ai_game.stats
 
         # Ustawienia czcionki.
         self.text_color = (60, 60, 60)
         self.font = pygame.font.SysFont(None, 48)
 
-        # Przygotowanie początkowych obrazów z punktacją.
+        # Przygotowanie początkowych obrazów z punktacją i poziomem gry.
         self.prep_score()
         self.prep_high_score()
+        self.prep_level()
 
     def prep_score(self):
         """Umieszczenie aktualnego wyniku gracza w prawym górnym rogu ekranu."""
         rounded_score = round(self.stats.score, -1)
         score_str = "{:,}".format(rounded_score)
-        self.score_image = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
+        self.score_image = self.font.render(score_str, True, self.text_color)
 
         self.score_rect = self.score_image.get_rect()
         self.score_rect.right = self.screen_rect.right - 20
@@ -33,11 +33,20 @@ class Scoreboard:
         """Umieszczenie najlepszego wyniku gry na środku ekranu, przy górnej krawędzi."""
         rounded_high_score = round(self.stats.high_score, -1)
         high_score_str = "{:,}".format(rounded_high_score)
-        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color)
 
         self.high_score_rect = self.high_score_image.get_rect()
         self.high_score_rect.centerx = self.screen_rect.centerx
         self.high_score_rect.top = self.score_rect.top
+
+    def prep_level(self):
+        """Umieszczenie aktualnego poziomu gry w prawym górnym rogu ekranu, pod punktacją."""
+        level_str = str(self.stats.level)
+        self.level_image = self.font.render(level_str, True, self.text_color)
+
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
 
     def check_high_score(self):
         """Sprawdzenie, czy mamy nowy najlepszy wynik osiągnięty dotąd w grze."""
@@ -49,3 +58,4 @@ class Scoreboard:
         """Wyświetlenie punktacji na ekranie."""
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
